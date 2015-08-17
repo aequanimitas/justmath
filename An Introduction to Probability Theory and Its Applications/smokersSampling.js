3bcb35e2b9ad8130241ac692d5ff85a64a619892
metalfatigue <htavenido@gmail.com>
2015-08-17 11:42:40 +0800

    update code to use array

--- An Introduction to Probability Theory and Its Applications/smokersSampling.js
+++ An Introduction to Probability Theory and Its Applications/smokersSampling.js
@@ -1,12 +1,19 @@
 var count = 0;
+var samplePoints = [];
 for (var x = 0; x < 101; x += 1) {
   for (var y = 0; y < 101; y += 1) {
     for (var a = 0; a < 101; a += 1) {
       for (var b = 0; b < 101; b += 1) {
-        if ((x + y + a + b) == 100) { count += 1 }
+        if ((x + y + a + b) == 100) { 
+          samplePoints.push({
+            Msmokers: x,
+            MnonSmokers: y,
+            Fsmokers: a,
+            FnonSmokers: b
+          });
+        }
       }
     }
   }
 }
-
-console.log(count);
+console.log(samplePoints.length);
