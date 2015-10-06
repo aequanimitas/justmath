7c1ee32f48441d95d4e45d8a8ebdb3a8ba835c61
metalfatigue <htavenido@gmail.com>
2015-10-07 04:21:36 +0800

    quick commit

--- IP2E/example1_3.py
+++ IP2E/example1_3.py
@@ -2,13 +2,13 @@ import operator
 
 sample_space = [(x,y) for x in range(1,5,1) for y in range(1,5,1)]
 
-def sumRollsEven(x, oper=None):
+def sum_rolls_even(x, oper=None):
     if oper:
         return oper(sum(x) % 2 == 0)
     else:
         return sum(x) % 2 == 0
 
-def firstRoll(x, comp=None):
+def first_roll(x, comp=None):
     if comp:
         x,y = x
         return comp(x,y)
@@ -24,10 +24,10 @@ def generateEventSpace(lst, cond=None, oper=None):
     else:
         return [x for x in lst]
 
-sumOfRollsEven = generateEventSpace(sample_space, sumRollsEven)
-sumOfRollsOdd = generateEventSpace(sample_space, sumRollsEven, operator.not_)
-firstRollEqualSecond = generateEventSpace(sample_space, firstRoll, operator.eq)
-firstRollLargerSecond = generateEventSpace(sample_space, firstRoll, operator.gt)
+sum_of_rolls_even = generateEventSpace(sample_space, sum_rolls_even)
+sum_of_rolls_odd = generateEventSpace(sample_space, sum_rolls_even, operator.not_)
+first_roll_equal_second = generateEventSpace(sample_space, first_roll, operator.eq)
+first_roll_larger_second = generateEventSpace(sample_space, first_roll, operator.gt)
 atleast_on_roll_equal_four = [x for x in sample_space if (x[0] == 4 or x[1] == 4)]
 
 ## using numpy
@@ -51,10 +51,11 @@ grid[2,1:] = [(x, 2) for x in range(1,5,1)]
 grid[1,1:] = [(x, 3) for x in range(1,5,1)]
 grid[0,1:] = [(x, 4) for x in range(1,5,1)]
 
-## just list comprehension
 np_sore = sorted([x for a in grid[:] for x in a if type(x) != int and sum(x) % 2 == 0]) == \
           sorted([v for i, v in np.ndenumerate(grid) if type(v) != int and sum(v) % 2 == 0]) == \
-          sumOfRollsEven
+          sum_of_rolls_even == \
+          sorted([b for a in grid[:4:,[1,2,3,4]] for b in a if sum(b) % 2 == 0]) == \
+          sorted(filter(sum_rolls_even, [b for a in grid[:4:,[1,2,3,4]] for b in a]))
 
 # np_soro = [x for a in grid[:] for x in a if type(x) != int and sum(x) % 2 != 0]
 
