7b5ee447961336cba9258bfca326173c71fe0d7b
metalfatigue <htavenido@gmail.com>
2015-10-13 03:48:08 +0800

    Basic PUV NCR data queries - seperate origin and destination for routes

--- ph/puv_mm_rizal.py
+++ ph/puv_mm_rizal.py
@@ -2,9 +2,19 @@ import pandas as pd
 
 rizal_area = "Binangonan|Cainta|Taytay|Antipolo|Angono|Tanay"
 
-rizal_routes = pd.read_csv("data/routespublicutilityvehiclesmetromanila.csv")
+ncr_routes_data = pd.read_csv("data/routespublicutilityvehiclesmetromanila.csv")
 
 def routes_by_area(cond_area):
-    return rizal_routes[rizal_routes["route"].str.contains(cond_area)].groupby("puv")
+    return ncr_routes_data[ncr_routes_data["route"].str.contains(cond_area)].groupby("puv")
 
 print routes_by_area(rizal_area).count()
+
+# seperate origins and destination into their own columns
+
+ncr_routes_data["origin"] = pd.Series(
+                                ncr_routes_data["route"].str.split(" - ").str[0],
+                                index=ncr_routes_data.index)
+
+ncr_routes_data["destination"] = pd.Series(
+                                ncr_routes_data["route"].str.split(" - ").str[1],
+                                index=ncr_routes_data.index)
