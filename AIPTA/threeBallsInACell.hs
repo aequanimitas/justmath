37d902f3e91931633c1958a04adda15ac3d60397
aequanimitas <htavenido@gmail.com>
2016-03-02 23:55:48 +0800

    event A and B done, event C not yet

--- AIPTA/threeBallsInACell.hs
+++ AIPTA/threeBallsInACell.hs
@@ -1,4 +1,11 @@
 sampleSpace = [["abc","",""], ["","abc",""], ["","","abc"],["ab","c",""], ["ac","b",""], ["bc","a",""], ["ab","","c"], ["ac","","b"], ["bc","","a"], ["a","bc",""], ["b","ac",""], ["c","ab",""], ["a","","bc"], ["b","","ac"], ["c","","ab"], ["","ab","c"], ["","ac","b"], ["","bc","a"], ["","a","bc"], ["","b","ac"], ["","c","ab"],["a","b","c"], ["a","c","b"], ["b","a","c"], ["b","c","a"], ["c","a","b"], ["c","b","a"]]
 
+-- should be 21
+eventAPredicate x = length x >= 2
+eventA = [x | x <- sampleSpace, any eventAPredicate x]
+
 eventBPredicate x = length(x !! 0) > 0
 eventB = [x | x <- sampleSpace, eventBPredicate x]
+
+eventCPredicate x = (eventBPredicate x) && (eventAPredicate x)
+eventC = [x | x <- sampleSpace, eventBPredicate x, eventAPredicate x]
