4d5109df9e8237953faaef86d0db20dfb85348e5
aequanimitas <htavenido@gmail.com>
2016-03-04 05:42:43 +0800

    relation among events

--- AIPTA/0001.hs
+++ AIPTA/0001.hs
@@ -1,5 +1,12 @@
+import Data.List
 -- sum six
 dieRoll x = [1..x]
 
 sumSix = [[x,y] | x <- dieRoll 6, y <- dieRoll 6, x + y == 6]
 twoOddFace = [[x,y]| x <- dieRoll 6, y <- dieRoll 6, odd x && odd y]
+
+
+-- relation among events
+
+relUnion = [1..5] `union` [6..10]
+relIntersection = [1..10] `intersect` [5..15]
