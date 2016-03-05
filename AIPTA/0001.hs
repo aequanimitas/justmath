9a0322d2cffbeb7a47c6d6e2a2e376793e33f84b
aequanimitas <htavenido@gmail.com>
2016-03-05 12:28:03 +0800

    conditions, predicated for couples

--- AIPTA/0001.hs
+++ AIPTA/0001.hs
@@ -10,3 +10,12 @@ twoOddFace = [[x,y]| x <- dieRoll 6, y <- dieRoll 6, odd x && odd y]
 
 relUnion = [1..5] `union` [6..10]
 relIntersection = [1..10] `intersect` [5..15]
+relNotIn = ([1..10] \\ [1..5])
+occImpliesNonOcc = length([1..10] \\ [1..5])
+
+-- husband and wife example
+predicateA x = x > 40
+predicateB x y = x > y
+predicateC x y = x > 40 && y = 40
+
+-- apply predicate functions
