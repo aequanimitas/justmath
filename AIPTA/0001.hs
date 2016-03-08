51ea6ad0ed1ba0a50fcfffa18e18aace2b89aa8b
aequanimitas <htavenido@gmail.com>
2016-03-08 08:33:38 +0800

    examples for d4

--- AIPTA/0001.hs
+++ AIPTA/0001.hs
@@ -16,6 +16,20 @@ occImpliesNonOcc = length([1..10] \\ [1..5])
 -- husband and wife example
 predicateA x = x > 40
 predicateB x y = x > y
-predicateC x y = x > 40 && y = 40
+predicateC x y = x > 40 && y >= 40
 
--- apply predicate functions
+-- relation among events
+--
+sampleSpace = [["abc","",""], ["","abc",""], ["","","abc"],["ab","c",""], ["ac","b",""], ["bc","a",""], ["ab","","c"], ["ac","","b"], ["bc","","a"], ["a","bc",""], ["b","ac",""], ["c","ab",""], ["a","","bc"], ["b","","ac"], ["c","","ab"], ["","ab","c"], ["","ac","b"], ["","bc","a"], ["","a","bc"], ["","b","ac"], ["","c","ab"],["a","b","c"], ["a","c","b"], ["b","a","c"], ["b","c","a"], ["c","a","b"], ["c","b","a"]]
+
+cellLength x y = length x == y
+-- E1E2 = T3
+exDef4Fn a b = [x | x <- sampleSpace , cellLength (x !! a) b]
+e1Empty = exDef4Fn 0 0
+e2Empty = exDef4Fn 1 0
+
+triply3 = exDef4Fn 2 3
+
+simply3 = [x | x <- sampleSpace , cellLength (x !! 2) 1]
+simply2 = [x | x <- sampleSpace , cellLength (x !! 1) 1]
+simply1 = [x | x <- sampleSpace , cellLength (x !! 0) 1]
