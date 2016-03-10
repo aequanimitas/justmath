f97d88cb898eb200228355c197097c88bf2f7045
aequanimitas <htavenido@gmail.com>
2016-03-10 10:14:13 +0800

    put ILA under linear algebra

--- ILA/001.1_example.hs
+++
@@ -1,4 +0,0 @@
-addVector :: [Int] -> [Int] -> [Int]
-addVector x y
-  | length x == 1 && length y == 1  = [head(x) + head(y)]
-  | otherwise                       = head(x) + head(y) : addVector (drop 1 x) (drop 1 y)
--- ILA/README.md
+++
@@ -1,1 +0,0 @@
-Code for Introduction to Linear Algebra
