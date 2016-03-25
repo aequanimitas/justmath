b042d5fb0c657e0cd84b650748de8fd2db35cf1f
aequanimitas <htavenido@gmail.com>
2016-03-25 11:02:15 +0800

    add triangle rule

--- linearalgebra/vector_spaces.py
+++ linearalgebra/vector_spaces.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 
-fig = plt.figure(2)
+fig = plt.figure("Parallelogram Rule")
 fig.clf()
 ax = fig.add_subplot(111, autoscale_on=True, xlim=(-2, 2), ylim=(-2, 2))
 # args
 arrow_kwargs = {
     "head_width": 0.05,
     "head_length": 0.1,
-    "fc":'k',
-    "ec": 'k'
+    "fc": "#00BFFF",
+    "ec": "#00BFFF"
 }
 
 # plot parallelogram rule
@@ -17,9 +17,16 @@ ax.arrow(0, 0, 0.5, 0.7, **arrow_kwargs)
 ax.arrow(0, 0, 0.5, 0.3, **arrow_kwargs)
 
 # adjacent should be dashed
-adj_kwargs = arrow_kwargs
+adj_kwargs = arrow_kwargs.copy()
 adj_kwargs["linestyle"] = "dotted"
 
 ax.arrow(0.5, 0.3, 0.5, 0.7, **adj_kwargs)
 ax.arrow(0.5, 0.7, 0.5, 0.3, **adj_kwargs)
+
+fig2 = plt.figure("Triangle Rule")
+ay = fig2.add_subplot(111, autoscale_on=True, xlim=(-2, 2), ylim=(-2, 2))
+ay.arrow(0, 0, 0.5, 0.7, **arrow_kwargs)
+ay.arrow(0.5, 0.7, 0.5, 0.3, **arrow_kwargs)
+ay.arrow(0, 0, 1, 1, **arrow_kwargs)
+
 plt.show()
