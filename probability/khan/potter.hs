7f93575a2f3f5c50fbf003af6b4b14225cb4eef0
aequanimitas <htavenido@gmail.com>
2016-03-27 10:36:32 +0800

    move to own folder, add khan example

---
+++ probability/khan/potter.hs
@@ -0,0 +1,4 @@
+woodTypes = ["holly", "elm", "maple", "wenge"]
+coreMaterials = ["phoenix feather", "unicorn hair", "dragon scale", "raven feather", "thestral tail"]
+
+sampleSpace = [[x,y] | x <- woodTypes , y <- coreMaterials ]
