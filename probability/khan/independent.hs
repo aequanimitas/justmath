41ece5bc8b8839c3f576e96d6d1b4489c0ccb3a3
aequanimitas <htavenido@gmail.com>
2016-03-27 18:00:22 +0800

    add comprehension for diagram examples

--- probability/khan/independent.hs
+++ probability/khan/independent.hs
@@ -4,3 +4,6 @@ twoDieRolls = [(x,y) | x <- [1..6], y <- [1..6]]
 rollingDoublesPred x = (fst x) == (snd x) 
 
 rollingDoubles =  [x | x <- twoDieRolls , rollingDoublesPred x]
+
+-- for diagrams
+diags = [(x,y) | x <- [1..3], y <- [1..3]]
